12016c6dc2cd0d3cd6c029a9e5055fdf971176f0
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-01 15:03:25 +0300

    Create function for displaying array elements

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ for (let i = 0; i < 10; i++) {
     array1.push(Math.round(Math.random() * 100));
 }
 
+// 1. Функция принимает массив и выводит его на экран.
+console.log('1. Функция принимает массив и выводит его на экран');
+
+/**
+ *
+ * @param arr
+ */
+function displayArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+}
+
+displayArray(array1);
+
